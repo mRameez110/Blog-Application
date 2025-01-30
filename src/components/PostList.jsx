456dcfae3f4a3ecb27dcddbc66a37c0e9d27b809
mRameez110 <temprary87@gmail.com>
2025-01-30 21:24:28 +0500

    post list component

---
+++ src/components/PostList.jsx
@@ -0,0 +1,19 @@
+import PostListItems from "./PostListItems"
+
+const PostList = () => {
+  return (
+
+    <>
+    <PostListItems />
+    <PostListItems />
+    <PostListItems />
+    <PostListItems />
+    <PostListItems />
+    <PostListItems />
+    <PostListItems />
+    <PostListItems />
+    </>
+  )
+}
+
+export default PostList
