fd27378ad37b194708d1c4275a77dd5de4b5dee6
mRameez110 <temprary87@gmail.com>
2025-01-30 16:38:35 +0500

    Modified Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,80 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import { IKImage } from "imagekitio-react";
+
+import Image from "./Image";
+import { Link } from "react-router-dom";
+
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
 
 function Navbar() {
-
-const[open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
 
   return (
-    <div className='w-full h-16 md:h-20 flex items-center justify-between'>
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      {/* LOGO */}
+      <Link to="/" className="flex items-center gap-4 text-2xl font-bold ">
+        {/* <img src="/logo.png" className="w-8 h-8" alt="" /> */}
+        <Image src="logo.png" alt="Logo" w={32} h={32} />
+        <span>lama logo</span>
+      </Link>
 
-        {/* LOGO */}
+      {/* Mobile MENU */}
 
-        <div className='flex items-center gap-4 '>
-            <img src="/logo.png" className='w-8 h-8' alt="" />
-            <span>lama logo</span>
+      <div className="md:hidden">
+        {/* MOBILE BUTTON */}
+
+        <div
+          className="cursor-pointer text-4xl"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          {open ? "X" : "="}
         </div>
 
+        {/* MOBILE LINK LIST */}
 
-        {/* Mobile MENU */}
-
-        <div className='md:hidden'>
-
-
-            {/* MOBILE BUTTON */}
-
-            <div className='cursor-pointer text-4xl' onClick={()=>setOpen((prev)=> !prev)}>
-                {open ? "X" : "="}
-            </div>
-
-            {/* MOBILE LINK LIST */}
-
-
-            <div className={`w-full h-screen flex flex-col items-center justify-center absolute top-16 bg-red-700 transition-all ease-in-out ${open ? '-right-0' :'-right-[100%'}`}> 
-                menu
-            </div>
+        <div
+          className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${
+            open ? "-right-0" : "-right-[100%]"
+          }`}
+        >
+          <Link to="/">Home</Link>
+          <Link to="/">Trending</Link>
+          <Link to="/">Most Popular</Link>
+          <Link to="/">About</Link>
+          <Link to="/">
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+              Login
+            </button>
+          </Link>
         </div>
+      </div>
 
-        {/* DESKTOP MENU */}
+      {/* DESKTOP MENU */}
 
-        <div className='hidden md:flex'>D</div>
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <Link to="/">Home</Link>
+        <Link to="/">Trending</Link>
+        <Link to="/">Most Popular</Link>
+        <Link to="/">About</Link>
+        <SignedOut>
+          <Link to="/login">
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+              Login
+            </button>
+          </Link>
+        </SignedOut>
 
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
