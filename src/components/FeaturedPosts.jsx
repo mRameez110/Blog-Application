a678c1b2eebece73e8ad6492537a7bb0822ab58c
mRameez110 <temprary87@gmail.com>
2025-01-30 16:47:29 +0500

    feat: featured product component

---
+++ src/components/FeaturedPosts.jsx
@@ -0,0 +1,15 @@
+const FeaturedPosts = () => {
+  return (
+    <div className="mt-8 flex flex-col lg:flex-row gap-8">FeaturedPosts
+    
+    {/* First */}
+
+    <div className="w-full lg:w-1/2 flex flex-col gap-4"> </div>
+
+    {/* Others */}
+    <div className="w-full lg:w-1/2 flex flex-col gap-4"></div>
+    </div>
+  )
+}
+
+export default FeaturedPosts
