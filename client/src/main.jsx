c00dad237178791b0755b5f8d9ee225747a6ab31
mRameez110 <temprary87@gmail.com>
2025-02-03 19:43:17 +0500

    modify main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,83 +1,74 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
 
-import Homepage from './routes/Homepage.jsx';
-import PostListPage from './routes/PostListPage.jsx';
-import Write from './routes/Write.jsx';
-import LoginPage from './routes/LoginPage.jsx';
-import RegisterPage from './routes/RegisterPage.jsx';
-import SinglePostPage from './routes/SinglePostPage.jsx';
-import MainLayout from './layouts/MainLayout.jsx';
-import { ClerkProvider } from '@clerk/clerk-react';
+import Homepage from "./routes/Homepage.jsx";
+import PostListPage from "./routes/PostListPage.jsx";
+import Write from "./routes/Write.jsx";
+import LoginPage from "./routes/LoginPage.jsx";
+import RegisterPage from "./routes/RegisterPage.jsx";
+import SinglePostPage from "./routes/SinglePostPage.jsx";
+import MainLayout from "./layouts/MainLayout.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ToastContainer } from "react-toastify";
+const queryClient = new QueryClient();
 
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
-
+  throw new Error("Missing Publishable Key");
 }
 
 const router = createBrowserRouter([
-
   {
-    
-    element:<MainLayout />,
+    element: <MainLayout />,
 
     children: [
-
-
       {
-        path:'/',
-        element: <Homepage />
+        path: "/",
+        element: <Homepage />,
       },
 
       {
-        path:'/posts',
-        element: <PostListPage />
+        path: "/posts",
+        element: <PostListPage />,
       },
-    
+
       {
-        path:'/:slug',
-        element: <SinglePostPage />
+        path: "/:slug",
+        element: <SinglePostPage />,
       },
-    
+
       {
-        path:'/write',
-        element: <Write />
+        path: "/write",
+        element: <Write />,
       },
-    
+
       {
-        path:'/login',
-        element: <LoginPage />
+        path: "/login",
+        element: <LoginPage />,
       },
-    
+
       {
-        path:'/register',
-        element: <RegisterPage />
+        path: "/register",
+        element: <RegisterPage />,
       },
-    ]
+    ],
   },
+]);
 
-  
-
-
-])
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-    <RouterProvider router={router} />
-    {/* <App /> */}
-
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ToastContainer position="bottom-right" />
+        {/* <App /> */}
+      </QueryClientProvider>
     </ClerkProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
