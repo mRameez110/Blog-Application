fe83c83356d250a7a9895966cf967137fecd111c
mRameez110 <temprary87@gmail.com>
2025-02-03 20:23:47 +0500

    Revert "Revert "modify HomePage.jsx""
    This reverts commit 551766328fc41c695ec7271a23574752a38496c6.

--- client/src/routes/Homepage.jsx
+++ client/src/routes/Homepage.jsx
@@ -1,82 +1,195 @@
-import { Link } from "react-router-dom";
-import MainCategories from "../components/MainCategories";
-import FeaturedPosts from "../components/FeaturedPosts";
-import PostList from "../components/PostList";
+import { Link, useParams } from "react-router-dom";
+import Image from "../components/Image";
+import PostMenuActions from "../components/PostMenuActions";
+import Search from "../components/Search";
+import Comments from "../components/Comments";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { format } from "timeago.js";
 
-const Homepage = () => {
+const fetchPost = async (slug) => {
+  const res = await axios.get(`${import.meta.env.VITE_API_URL}/posts/${slug}`);
+  return res.data;
+};
+
+const SinglePostPage = () => {
+  const { slug } = useParams();
+
+  const { isPending, error, data } = useQuery({
+    queryKey: ["post", slug],
+    queryFn: () => fetchPost(slug),
+  });
+
+  if (isPending) return "loading...";
+  if (error) return "Something went wrong!" + error.message;
+  if (!data) return "Post not found!";
+
   return (
-    <div className="mt-4 flex flex-col gap-4">
-      Homepage
-      {/* BREADCRUMB */}
-      <div className="flex gap-4">
-        <Link to="/home"> Home</Link>
-
-        <span>.</span>
-        <span className="text-bluw-800">Blogs and Articles</span>
-      </div>
-      {/* INTRODUCTION */}
-      <div className="flex items-center justify-between">
-        <div className="">
-          <h1 className="text-gray-800 text-2xl md:text-5xl lg: text-6xl font-bold">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    <div className="flex flex-col gap-8">
+      {/* detail */}
+      <div className="flex gap-8">
+        <div className="lg:w-3/5 flex flex-col gap-8">
+          <h1 className="text-xl md:text-3xl xl:text-4xl 2xl:text-5xl font-semibold">
+            {data.title}
           </h1>
-          <p className="mt-8  text-md md:text-xl">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nihil
-            assumenda autem, commodi excepturi exercitationem laudantium
-            obcaecati voluptatem magni
+          <div className="flex items-center gap-2 text-gray-400 text-sm">
+            <span>Written by</span>
+            <Link className="text-blue-800">{data.user.username}</Link>
+            <span>on</span>
+            <Link className="text-blue-800">{data.category}</Link>
+            <span>{format(data.createdAt)}</span>
+          </div>
+          <p className="text-gray-500 font-medium">{data.desc}</p>
+        </div>
+        {data.img && (
+          <div className="hidden lg:block w-2/5">
+            <Image src={data.img} w="600" className="rounded-2xl" />
+          </div>
+        )}
+      </div>
+      {/* content */}
+      <div className="flex flex-col md:flex-row gap-12 justify-between">
+        {/* text */}
+        <div className="lg:text-lg flex flex-col gap-6 text-justify">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias neque
+            fugiat itaque quas esse sunt cupiditate possimus cumque asperiores,
+            dolorem, dolores eligendi amet perferendis illum repellat nam quam
+            facilis veritatis. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sint ipsa fuga nihil numquam, quam dicta quas
+            exercitationem aliquam maxime quaerat, enim autem culpa sequi at!
+            Earum facere in ducimus culpa. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Libero fuga modi amet error aliquid
+            eos nobis vero soluta facilis, voluptatem, voluptates quod suscipit
+            obcaecati voluptate quaerat laborum, voluptatum dicta ipsum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias neque
+            fugiat itaque quas esse sunt cupiditate possimus cumque asperiores,
+            dolorem, dolores eligendi amet perferendis illum repellat nam quam
+            facilis veritatis. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sint ipsa fuga nihil numquam, quam dicta quas
+            exercitationem aliquam maxime quaerat, enim autem culpa sequi at!
+            Earum facere in ducimus culpa. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Libero fuga modi amet error aliquid
+            eos nobis vero soluta facilis, voluptatem, voluptates quod suscipit
+            obcaecati voluptate quaerat laborum, voluptatum dicta ipsum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias neque
+            fugiat itaque quas esse sunt cupiditate possimus cumque asperiores,
+            dolorem, dolores eligendi amet perferendis illum repellat nam quam
+            facilis veritatis. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sint ipsa fuga nihil numquam, quam dicta quas
+            exercitationem aliquam maxime quaerat, enim autem culpa sequi at!
+            Earum facere in ducimus culpa. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Libero fuga modi amet error aliquid
+            eos nobis vero soluta facilis, voluptatem, voluptates quod suscipit
+            obcaecati voluptate quaerat laborum, voluptatum dicta ipsum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias neque
+            fugiat itaque quas esse sunt cupiditate possimus cumque asperiores,
+            dolorem, dolores eligendi amet perferendis illum repellat nam quam
+            facilis veritatis. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sint ipsa fuga nihil numquam, quam dicta quas
+            exercitationem aliquam maxime quaerat, enim autem culpa sequi at!
+            Earum facere in ducimus culpa. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Libero fuga modi amet error aliquid
+            eos nobis vero soluta facilis, voluptatem, voluptates quod suscipit
+            obcaecati voluptate quaerat laborum, voluptatum dicta ipsum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias neque
+            fugiat itaque quas esse sunt cupiditate possimus cumque asperiores,
+            dolorem, dolores eligendi amet perferendis illum repellat nam quam
+            facilis veritatis. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sint ipsa fuga nihil numquam, quam dicta quas
+            exercitationem aliquam maxime quaerat, enim autem culpa sequi at!
+            Earum facere in ducimus culpa. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Libero fuga modi amet error aliquid
+            eos nobis vero soluta facilis, voluptatem, voluptates quod suscipit
+            obcaecati voluptate quaerat laborum, voluptatum dicta ipsum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias neque
+            fugiat itaque quas esse sunt cupiditate possimus cumque asperiores,
+            dolorem, dolores eligendi amet perferendis illum repellat nam quam
+            facilis veritatis. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sint ipsa fuga nihil numquam, quam dicta quas
+            exercitationem aliquam maxime quaerat, enim autem culpa sequi at!
+            Earum facere in ducimus culpa. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Libero fuga modi amet error aliquid
+            eos nobis vero soluta facilis, voluptatem, voluptates quod suscipit
+            obcaecati voluptate quaerat laborum, voluptatum dicta ipsum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias neque
+            fugiat itaque quas esse sunt cupiditate possimus cumque asperiores,
+            dolorem, dolores eligendi amet perferendis illum repellat nam quam
+            facilis veritatis. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Sint ipsa fuga nihil numquam, quam dicta quas
+            exercitationem aliquam maxime quaerat, enim autem culpa sequi at!
+            Earum facere in ducimus culpa. Lorem ipsum dolor sit amet
+            consectetur, adipisicing elit. Libero fuga modi amet error aliquid
+            eos nobis vero soluta facilis, voluptatem, voluptates quod suscipit
+            obcaecati voluptate quaerat laborum, voluptatum dicta ipsum.
           </p>
         </div>
-
-        {/* animated button */}
-
-        <Link to="write" className="hidden md:block relative">
-          <svg
-            viewBox="0 0 200 200"
-            width="200"
-            height="200"
-            className="text-lg tracking-widest animate-spin animatedButton"
-          >
-            <path
-              id="circlePath"
-              fill="none"
-              d="M 100, 100 m -75, 0 a 75,75 0 1,1 150,0 a 75,75 0 1,1 -150,0"
-            />
-            <text>
-              <textPath href="#circlePath" startOffset="0%">
-                Write your story •
-              </textPath>
-              <textPath href="#circlePath" startOffset="50%">
-                Share your idea •
-              </textPath>
-            </text>
-          </svg>
-          <button className="absolute top-0 left-0 right-0 bottom-0 m-auto w-20 h-20 bg-blue-800 rounded-full flex items-center justify-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              width="50"
-              height="50"
-              fill="none"
-              stroke="white"
-              strokeWidth="2"
-            >
-              <line x1="6" y1="18" x2="18" y2="6" />
-              <polyline points="9 6 18 6 18 15" />
-            </svg>
-          </button>
-        </Link>
+        {/* menu */}
+        <div className="px-4 h-max sticky top-8">
+          <h1 className="mb-4 text-sm font-medium">Author</h1>
+          <div className="flex flex-col gap-4">
+            <div className="flex items-center gap-8">
+              {data.user.img && (
+                <Image
+                  src={data.user.img}
+                  className="w-12 h-12 rounded-full object-cover"
+                  w="48"
+                  h="48"
+                />
+              )}
+              <Link className="text-blue-800">{data.user.username}</Link>
+            </div>
+            <p className="text-sm text-gray-500">
+              Lorem ipsum dolor sit amet consectetur
+            </p>
+            <div className="flex gap-2">
+              <Link>
+                <Image src="facebook.svg" />
+              </Link>
+              <Link>
+                <Image src="instagram.svg" />
+              </Link>
+            </div>
+          </div>
+          <PostMenuActions post={data} />
+          <h1 className="mt-8 mb-4 text-sm font-medium">Categories</h1>
+          <div className="flex flex-col gap-2 text-sm">
+            <Link className="underline">All</Link>
+            <Link className="underline" to="/">
+              Web Design
+            </Link>
+            <Link className="underline" to="/">
+              Development
+            </Link>
+            <Link className="underline" to="/">
+              Databases
+            </Link>
+            <Link className="underline" to="/">
+              Search Engines
+            </Link>
+            <Link className="underline" to="/">
+              Marketing
+            </Link>
+          </div>
+          <h1 className="mt-8 mb-4 text-sm font-medium">Search</h1>
+          <Search />
+        </div>
       </div>
-      {/* CATAGORIES */}
-      <MainCategories />
-      {/* FEATURED POSTS */}
-      <FeaturedPosts />
-      {/* POST LIST */}
-      <div className="">
-        <h1 className="my-8 text-2xl text-gray-600 ">Recent Posts</h1>
-        <PostList />
-      </div>
+      <Comments postId={data._id} />
     </div>
   );
 };
 
-export default Homepage;
+export default SinglePostPage;
